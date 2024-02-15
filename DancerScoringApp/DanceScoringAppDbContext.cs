2e1c32b2052b9babd7dfc7461682266ebe4212c3
Ada <ada.barteczko@op.pl>
2024-02-15 23:47:26 +0100

    Added entities

---
+++ DancerScoringApp/DanceScoringAppDbContext.cs
@@ -0,0 +1,36 @@
+﻿using DancerScoringApp.Entities;
+using Microsoft.EntityFrameworkCore;
+
+public class DancerScoringDbContext : DbContext
+{
+    public DbSet<AgeCategory> AgeCategories { get; set; }
+    public DbSet<Bonus> Bonuses { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Coach> Coaches { get; set; }
+    public DbSet<Competition> Competitions { get; set; }
+    public DbSet<Dancer> Dancers { get; set; }
+    public DbSet<JudgeRating> JudgeRatings { get; set; }
+    public DbSet<PenaltyPoints> PenaltyPoints { get; set; }
+    public DbSet<PenaltyPointsRating> PenaltyPointsRatings { get; set; }
+    public DbSet<RatingBonus> RatingBonuses { get; set; }
+    public DbSet<Role> Roles { get; set; }
+    public DbSet<Routine> Routines { get; set; }
+    public DbSet<Squad> Squads { get; set; }
+    public DbSet<SquadDancer> SquadDancers { get; set; }
+    public DbSet<Team> Teams { get; set; }
+    public DbSet<TeamCoach> TeamCoaches { get; set; }
+    public DbSet<TechJudgeRating> TechJudgeRatings { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<UserCompetition> UserCompetitions { get; set; }
+    public DbSet<UserRole> UserRoles { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(
+            "Host=localhost;Port=5432;Database=DanceScoringApp;Username=postgres;Password=P@ssw0rd");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+    }
+}
--- DancerScoringApp/Views/MainWindow.xaml.cs
+++ DancerScoringApp/Views/MainWindow.xaml.cs
@@ -23,5 +23,6 @@ namespace DancerScoringApp
         {
             InitializeComponent();
         }
+
     }
 }
